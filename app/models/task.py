1f1adac860f3a5f98e1afba5be4fedec5dc54e80
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-05-02 17:08:56 -0700

    Implemented helper function

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,21 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] 
+    description:  Mapped[str]
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.is_complete
+        } 
+
+    @classmethod
+    def from_dict(cls, dict_data_task):
+        return cls(title=dict_data_task["title"], description=["description"])
