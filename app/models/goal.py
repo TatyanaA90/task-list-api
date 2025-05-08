74b65de782345e01a18971e2efbf59a2378f5ed2
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-05-07 22:31:20 -0700

    Create goal model and migration

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,19 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+
+    def to_dict(self):
+        return { 
+            "id" : self.id,
+            "title": self.title,
+        } 
+
+    @classmethod
+    def from_dict(cls, dict_data_task):
+        return cls(title=dict_data_task["title"])
+    
+    def update_from_dict(self, dict_data_task):
+        if "title" in  dict_data_task:
+            self.title = dict_data_task["title"]
+
